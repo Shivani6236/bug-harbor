d4759085cd779dadd369528ed8c8a40710f74338
Shivani6236 <125167871+Shivani6236@users.noreply.github.com>
2023-10-10 19:02:06 +0530

    Update mongodb.js

--- database/config/mongodb.js
+++ database/config/mongodb.js
@@ -3,8 +3,8 @@ const url = 'mongodb+srv://shivani:BRiUiJ4FAdYZloPr@cluster0.5nm8wnz.mongodb.net
 
 async function getEmpData() {
     let client = await MongoClient.connect(url);
-    let connection = client.db('issue_tracker'); // Establish connection
-    return connection.collection('IssueTracker'); // creating a collection and naming it also
+    let connection = client.db('bug_harbor'); // Establish connection
+    return connection.collection('BugHarbor'); // creating a collection and naming it also
 }
 
 module.exports = getEmpData;
